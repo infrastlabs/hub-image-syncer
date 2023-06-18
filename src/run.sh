4755fea06cad39032e7ec0b7393dcbeb494b031c
sam#gemmi-win10 <sam@china.gz>
2023-06-18 12:43:39 +0800

    up src/run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,8 @@
 
+#!/bin/bash
+cur=$(cd "$(dirname "$0")"; pwd)
+cd $cur
+
 # file=image-syncer-v1.3.1-linux-amd64.tar.gz
 # https://ghproxy.com/https://github.com/AliyunContainerService/image-syncer/releases/download/v1.3.1/$file
 
